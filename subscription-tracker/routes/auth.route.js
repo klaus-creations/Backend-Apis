c2847765de46b42267474ae6d1c3ade5be8559b0
Abush Natanem <abushnatanem@gmail.com>
2025-02-26 22:04:46 +0300

    add the controllers  to the route

--- subscription-tracker/routes/auth.route.js
+++ subscription-tracker/routes/auth.route.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 const authRouter = Router();
 
-authRouter.post("sign-up", (req, res) => res.send({ title: "sign up" }));
-authRouter.post("/sign-in", (req, res) => res.send({ title: "sign in" }));
-authRouter.post("/sign-out", (req, res) => res.send({ title: "sign out" }));
+import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
+
+authRouter.post("sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
+
+export default authRouter;
