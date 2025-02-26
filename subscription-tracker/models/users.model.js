852d27e57bc750d146e1b3e082a0c50ee7e05d23
Abush Natanem <abushnatanem@gmail.com>
2025-02-26 16:52:48 +0300

    update user model export

--- subscription-tracker/models/users.model.js
+++ subscription-tracker/models/users.model.js
@@ -28,5 +28,5 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
-export default User;
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
