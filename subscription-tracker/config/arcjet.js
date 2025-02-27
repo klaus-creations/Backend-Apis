cd3869fd76cac23d1a17341e0d379bb681f74bfb
Abush Natanem <abushnatanem@gmail.com>
2025-02-27 21:50:00 +0300

    add the arcjet configuration

---
+++ subscription-tracker/config/arcjet.js
@@ -0,0 +1,23 @@
+import {} from "@arcjet/node";
+import arcjet, { detectBot, shield, tokenBucket } from "arcjet";
+import { ARCJET_KEY } from "./env";
+
+const aj = arcjet({
+  key: ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
+
+export default aj;
